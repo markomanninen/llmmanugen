1022f6bfe763b769f8dee70ccd992c9549d68c22
Marko Manninen <elonmedia@gmail.com>
2023-09-11 19:27:11 +0300

    init prompt class and setup for packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llmmanugen',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add console scripts here, if any
+        ],
+    },
+)
